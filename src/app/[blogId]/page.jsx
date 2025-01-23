3250e2538848e7da9928e42eb85a5ed3398fbc90
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-01-23 21:34:36 +0600

    add footer components

--- src/app/[blogId]/page.jsx
+++ src/app/[blogId]/page.jsx
@@ -7,14 +7,16 @@ export default async function blogDetails({ params }) {
     const post = await data.json()
     console.log(post)
     return (
-        <div className="card bg-base-100 w-96 shadow-xl mx-auto">
-            <div className="card-body">
-                <h2 className="card-title">{post.title}</h2>
-                <p>{post.body}</p>
-                <div className="card-actions justify-end">
-                    <BackButton />
+        <main className="min-h-screen">
+            <div className="card bg-base-100 w-96 shadow-xl mx-auto">
+                <div className="card-body">
+                    <h2 className="card-title">{post.title}</h2>
+                    <p>{post.body}</p>
+                    <div className="card-actions justify-end">
+                        <BackButton />
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
     )
 }
--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -5,7 +5,6 @@ export default function BlogCard({ post }) {
         <div className="card bg-base-100  shadow-xl">
             <div className="card-body">
                 <h2 className="card-title">{post.title}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div className="card-actions justify-end">
                     <Link href={`/${post.id}`}><button className="btn btn-primary">View Details</button></Link>
                 </div>
