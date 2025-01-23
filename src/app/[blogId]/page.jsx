eb0b41d132e4ea9d481c28d97d02e05ca99de5b9
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-01-23 21:29:39 +0600

    useRouter hook use

--- src/app/[blogId]/page.jsx
+++ src/app/[blogId]/page.jsx
@@ -1,3 +1,5 @@
+import BackButton from "@/components/BackButton"
+import { Link } from "lucide-react"
 
 export default async function blogDetails({ params }) {
     const { blogId } = params
@@ -5,8 +7,14 @@ export default async function blogDetails({ params }) {
     const post = await data.json()
     console.log(post)
     return (
-        <div>
-
+        <div className="card bg-base-100 w-96 shadow-xl mx-auto">
+            <div className="card-body">
+                <h2 className="card-title">{post.title}</h2>
+                <p>{post.body}</p>
+                <div className="card-actions justify-end">
+                    <BackButton />
+                </div>
+            </div>
         </div>
     )
 }
