65ac7d0b02544acbfeea838dceb3648b636cada9
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-01-24 12:27:43 +0600

    if user is not active then redirect to home route

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,4 +1,11 @@
-export default function Profile() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+export default async function Profile() {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    if (!user) {
+        redirect('/');
+    }
     return (
         <main className="min-h-screen">
             <h2 className="text-center py-10 text-orange-500 text-2xl font-semibold">"Welcome to your profile!"</h2>
