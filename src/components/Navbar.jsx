f86f7faeddbe8e84444c048ed8ca3442c7184848
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-01-24 19:05:41 +0600

    deploy app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ export default async function Navbar() {
                             {links}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-xl">Let's Blog</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
